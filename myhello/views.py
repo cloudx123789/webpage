bdca697e1a8d8459908fa24119154a3835a3babb
cloudx123789 <stephen0811@gmail.com>
2026-03-11 15:35:58 +0800

    Replace say_hello with myhello_api using DRF

--- myhello/views.py
+++ myhello/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def say_hello(request):
-    name = request.GET.get("name", "CGU")
-    return HttpResponse(f"Hello {name}")
+
+@api_view(['GET'])
+def myhello_api(request):
+
+    my_name = request.GET.get('name', None)
+
+    if my_name:
+        return Response(
+            {"data": "Hello " + my_name},
+            status=status.HTTP_200_OK
+        )
+
+    else:
+        return Response(
+            {"res": "parameter: name is None"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
